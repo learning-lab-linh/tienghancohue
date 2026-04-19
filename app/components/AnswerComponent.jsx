6e237b3b629507f69407e53c7e36bea04e450eaa
Khanhlinh952001 <vokhanhlinh952001@gmail.com>
2026-04-19 14:28:26 +0900

    feat: add quiz results JSON file and update AnswerComponent styles
    Introduced a new JSON file for quiz results to store user performance data. Updated the AnswerComponent styles to enhance visual clarity by refining class names and improving border and background color handling.

--- app/components/AnswerComponent.jsx
+++ app/components/AnswerComponent.jsx
@@ -12,7 +12,7 @@ export const AnswerComponent = ({
 }) => {
   const isSelected = userAnswer === (optionIndex + 1).toString();
   const isCorrect = correctAnswer === (optionIndex + 1).toString();
-  const labelClassNames = `block w-full rounded-lg bg-slate-100 border border-slate-200 px-4 py-2 text-left text-sm transition sm:text-base
+  const labelClassNames = `block w-full rounded-lg border px-4 py-2 text-left text-sm transition sm:text-base
     ${
       isSelected
         ? showResults
@@ -20,7 +20,7 @@ export const AnswerComponent = ({
             ? "border-emerald-500 bg-emerald-100 font-semibold text-emerald-900"
             : "border-rose-500 bg-rose-100 font-semibold text-rose-900"
           : "border-blue-500 bg-blue-100 font-semibold text-blue-900"
-        : "bg-slate-100 text-slate-800  "
+        : "border-slate-200 bg-slate-100 text-slate-800"
     }`;
 
   return (
